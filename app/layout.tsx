ca87c04630e56d03b1da061df958c76c9fdd0454
Walter Furrer <walterfurrer.dev@gmail.com>
2025-09-08 18:42:39 -0400

    gettin' slick with it (card hover animation)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,28 @@
-import { Footer } from "@/components/footer";
-import { Navbar } from "@/components/navbar";
+// import { Footer } from "@/components/footer";
+// import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
-import { IBM_Plex_Mono, IBM_Plex_Sans, IBM_Plex_Serif } from "next/font/google";
+import { Lora, Manrope, Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const plexMono = IBM_Plex_Mono({
-  variable: "--font-plex-mono",
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const plexSans = IBM_Plex_Sans({
-  variable: "--font-plex-sans",
+const sans = Manrope({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
-const plexSerif = IBM_Plex_Serif({
-  variable: "--font-plex-serif",
+const serif = Lora({
+  variable: "--font-serif",
   subsets: ["latin"],
-  weight: "400",
+});
+
+const mono = Space_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
-  title: "Walter Furrer Portfolio",
+  title: "Walter Furrer",
   description:
     "A portfolio site for Walter Furrer, featuring projects, experience, articles, and contact information.",
 };
@@ -34,9 +33,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="h-full">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${plexSans.variable} ${plexMono.variable} ${plexSerif.variable} flex h-full min-h-screen flex-col`}
+        className={`${sans.variable} ${serif.variable} ${mono.variable} flex min-h-screen flex-col`}
       >
         <div className="flex flex-1 flex-col">
           <ThemeProvider
@@ -45,10 +44,10 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <div className="container mx-auto flex max-w-screen-md flex-1 flex-col px-4">
-              <Navbar />
-              <main className="flex-1">{children}</main>
-              <Footer />
+            <div className="container mx-auto flex max-w-screen-sm flex-1 flex-col px-4">
+              {/* <Navbar /> */}
+              <main className="my-32 flex-1">{children}</main>
+              {/* <Footer /> */}
             </div>
           </ThemeProvider>
         </div>
